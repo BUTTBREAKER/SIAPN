0102e05ed46e7460fa077cbbd73b1e0eb569cd5d
fadrian06 <franyeradriansanchez@gmail.com>
2024-11-05 21:10:06 -0400

    Added favicon

--- views/layouts/unlogged-layout.php
+++ views/layouts/unlogged-layout.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width" />
     <title><?= $title ?> ~ SIPAN</title>
     <base href="<?= str_replace('index.php', '', $_SERVER['SCRIPT_NAME']) ?>" />
-    <link rel="icon" href="./assets/images/favicon.jpg" />
+    <link rel="icon" href="./assets/images/favicon.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
   </head>
   <body>
